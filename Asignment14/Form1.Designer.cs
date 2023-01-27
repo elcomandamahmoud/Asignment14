48870aa8f81da6072863fa9b568a419489148c30
elcomandamahmoud <elcomandamahmoud2011@gmail.com>
2023-01-27 14:44:20 +0200

    Add project files.

---
+++ Asignment14/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Asignment14
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
+            this.ControlsContainer = new System.Windows.Forms.TableLayoutPanel();
+            this.TXT3_3 = new System.Windows.Forms.TextBox();
+            this.TXT2_3 = new System.Windows.Forms.TextBox();
+            this.TXT1_3 = new System.Windows.Forms.TextBox();
+            this.TXT3_2 = new System.Windows.Forms.TextBox();
+            this.TXT2_2 = new System.Windows.Forms.TextBox();
+            this.TXT1_2 = new System.Windows.Forms.TextBox();
+            this.TXT3_1 = new System.Windows.Forms.TextBox();
+            this.TXT2_1 = new System.Windows.Forms.TextBox();
+            this.BTN3 = new System.Windows.Forms.Button();
+            this.BTN2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTN1 = new System.Windows.Forms.Button();
+            this.ButtonsContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.BTNClear = new System.Windows.Forms.Button();
+            this.BTNSave = new System.Windows.Forms.Button();
+            this.BTNExit = new System.Windows.Forms.Button();
+            this.TXT1_1 = new System.Windows.Forms.TextBox();
+            this.Summery = new System.Windows.Forms.RichTextBox();
+            this.ControlsContainer.SuspendLayout();
+            this.ButtonsContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ControlsContainer
+            // 
+            resources.ApplyResources(this.ControlsContainer, "ControlsContainer");
+            this.ControlsContainer.Controls.Add(this.TXT3_3, 4, 4);
+            this.ControlsContainer.Controls.Add(this.TXT2_3, 3, 4);
+            this.ControlsContainer.Controls.Add(this.TXT1_3, 2, 4);
+            this.ControlsContainer.Controls.Add(this.TXT3_2, 4, 3);
+            this.ControlsContainer.Controls.Add(this.TXT2_2, 3, 3);
+            this.ControlsContainer.Controls.Add(this.TXT1_2, 2, 3);
+            this.ControlsContainer.Controls.Add(this.TXT3_1, 4, 2);
+            this.ControlsContainer.Controls.Add(this.TXT2_1, 3, 2);
+            this.ControlsContainer.Controls.Add(this.BTN3, 1, 4);
+            this.ControlsContainer.Controls.Add(this.BTN2, 1, 3);
+            this.ControlsContainer.Controls.Add(this.label3, 4, 1);
+            this.ControlsContainer.Controls.Add(this.label2, 3, 1);
+            this.ControlsContainer.Controls.Add(this.label1, 2, 1);
+            this.ControlsContainer.Controls.Add(this.BTN1, 1, 2);
+            this.ControlsContainer.Controls.Add(this.ButtonsContainer, 1, 6);
+            this.ControlsContainer.Controls.Add(this.TXT1_1, 2, 2);
+            this.ControlsContainer.Controls.Add(this.Summery, 1, 5);
+            this.ControlsContainer.Name = "ControlsContainer";
+            // 
+            // TXT3_3
+            // 
+            resources.ApplyResources(this.TXT3_3, "TXT3_3");
+            this.TXT3_3.Name = "TXT3_3";
+            // 
+            // TXT2_3
+            // 
+            resources.ApplyResources(this.TXT2_3, "TXT2_3");
+            this.TXT2_3.Name = "TXT2_3";
+            // 
+            // TXT1_3
+            // 
+            resources.ApplyResources(this.TXT1_3, "TXT1_3");
+            this.TXT1_3.Name = "TXT1_3";
+            // 
+            // TXT3_2
+            // 
+            resources.ApplyResources(this.TXT3_2, "TXT3_2");
+            this.TXT3_2.Name = "TXT3_2";
+            // 
+            // TXT2_2
+            // 
+            resources.ApplyResources(this.TXT2_2, "TXT2_2");
+            this.TXT2_2.Name = "TXT2_2";
+            // 
+            // TXT1_2
+            // 
+            resources.ApplyResources(this.TXT1_2, "TXT1_2");
+            this.TXT1_2.Name = "TXT1_2";
+            // 
+            // TXT3_1
+            // 
+            resources.ApplyResources(this.TXT3_1, "TXT3_1");
+            this.TXT3_1.Name = "TXT3_1";
+            // 
+            // TXT2_1
+            // 
+            resources.ApplyResources(this.TXT2_1, "TXT2_1");
+            this.TXT2_1.Name = "TXT2_1";
+            // 
+            // BTN3
+            // 
+            resources.ApplyResources(this.BTN3, "BTN3");
+            this.BTN3.Name = "BTN3";
+            this.BTN3.UseVisualStyleBackColor = true;
+            this.BTN3.Click += new System.EventHandler(this.BTN3_Click);
+            // 
+            // BTN2
+            // 
+            resources.ApplyResources(this.BTN2, "BTN2");
+            this.BTN2.Name = "BTN2";
+            this.BTN2.UseVisualStyleBackColor = true;
+            this.BTN2.Click += new System.EventHandler(this.BTN2_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // BTN1
+            // 
+            resources.ApplyResources(this.BTN1, "BTN1");
+            this.BTN1.Name = "BTN1";
+            this.BTN1.UseVisualStyleBackColor = true;
+            this.BTN1.Click += new System.EventHandler(this.BTN1_Click);
+            // 
+            // ButtonsContainer
+            // 
+            resources.ApplyResources(this.ButtonsContainer, "ButtonsContainer");
+            this.ControlsContainer.SetColumnSpan(this.ButtonsContainer, 4);
+            this.ButtonsContainer.Controls.Add(this.BTNClear);
+            this.ButtonsContainer.Controls.Add(this.BTNSave);
+            this.ButtonsContainer.Controls.Add(this.BTNExit);
+            this.ButtonsContainer.Name = "ButtonsContainer";
+            // 
+            // BTNClear
+            // 
+            resources.ApplyResources(this.BTNClear, "BTNClear");
+            this.BTNClear.Name = "BTNClear";
+            this.BTNClear.UseVisualStyleBackColor = true;
+            this.BTNClear.Click += new System.EventHandler(this.BTNClear_Click);
+            // 
+            // BTNSave
+            // 
+            resources.ApplyResources(this.BTNSave, "BTNSave");
+            this.BTNSave.Name = "BTNSave";
+            this.BTNSave.UseVisualStyleBackColor = true;
+            this.BTNSave.Click += new System.EventHandler(this.BTNSave_Click);
+            // 
+            // BTNExit
+            // 
+            resources.ApplyResources(this.BTNExit, "BTNExit");
+            this.BTNExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BTNExit.Name = "BTNExit";
+            this.BTNExit.UseVisualStyleBackColor = true;
+            this.BTNExit.Click += new System.EventHandler(this.BTNExit_Click);
+            // 
+            // TXT1_1
+            // 
+            resources.ApplyResources(this.TXT1_1, "TXT1_1");
+            this.TXT1_1.Name = "TXT1_1";
+            // 
+            // Summery
+            // 
+            resources.ApplyResources(this.Summery, "Summery");
+            this.ControlsContainer.SetColumnSpan(this.Summery, 4);
+            this.Summery.Name = "Summery";
+            // 
+            // HomeForm
+            // 
+            this.AcceptButton = this.BTNSave;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BTNExit;
+            this.Controls.Add(this.ControlsContainer);
+            this.Name = "HomeForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HomeForm_FormClosed);
+            this.ControlsContainer.ResumeLayout(false);
+            this.ControlsContainer.PerformLayout();
+            this.ButtonsContainer.ResumeLayout(false);
+            this.ButtonsContainer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel ControlsContainer;
+        private System.Windows.Forms.TextBox TXT3_3;
+        private System.Windows.Forms.TextBox TXT2_3;
+        private System.Windows.Forms.TextBox TXT1_3;
+        private System.Windows.Forms.TextBox TXT3_2;
+        private System.Windows.Forms.TextBox TXT2_2;
+        private System.Windows.Forms.TextBox TXT1_2;
+        private System.Windows.Forms.TextBox TXT3_1;
+        private System.Windows.Forms.TextBox TXT2_1;
+        private System.Windows.Forms.Button BTN3;
+        private System.Windows.Forms.Button BTN2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTN1;
+        private System.Windows.Forms.FlowLayoutPanel ButtonsContainer;
+        private System.Windows.Forms.Button BTNClear;
+        private System.Windows.Forms.Button BTNSave;
+        private System.Windows.Forms.Button BTNExit;
+        private System.Windows.Forms.TextBox TXT1_1;
+        private System.Windows.Forms.RichTextBox Summery;
+    }
+}
+
---
+++ Asignment14/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+namespace Asignment14
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+            Clear();
+            if (File.Exists(FileName))
+            {
+                StreamReader RestoreData = new StreamReader(FileName);
+string Rows=                RestoreData.ReadToEnd();
+                string Row1 = Rows.Split('\n')[0];
+                TXT1_1.Text = Row1.Split(',')[0];
+                TXT2_1.Text = Row1.Split(',')[1];
+                TXT3_1.Text = Row1.Split(',')[2];
+                string Row2 = Rows.Split('\n')[1];
+                TXT1_2.Text = Row2.Split(',')[0];
+                TXT2_2.Text = Row2.Split(',')[1];
+                TXT3_2.Text = Row2.Split(',')[2];
+                string Row3 = Rows.Split('\n')[2];
+                TXT1_3.Text = Row3.Split(',')[0];
+                TXT2_3.Text = Row3.Split(',')[1];
+                TXT3_3.Text = Row3.Split(',')[2];
+                RestoreData.Close();
+            }
+            FilSummery();
+        }
+        public void FilSummery()
+        {
+            Summery.ReadOnly = true;
+            Summery.Text = "لقد سَبَّحْتَ الله :" + TXT3_1.Text + "\n" + "لقد حَمِدّْتَ الله :" + TXT3_2.Text + "\n" + "لقد كَبَّرْتَ الله :" + TXT3_3.Text + "\n" + "إجمالي عدد التسبيحات هو: " +(int.Parse(TXT3_1.Text)+int.Parse(TXT3_2.Text)+int.Parse(TXT3_3.Text))+ " تسبيحة";
+        }
+        public void Clear()
+        {
+            foreach (Control c in ControlsContainer.Controls)
+            {
+                if (c is TextBox)
+                {
+                    ((TextBox)c).ReadOnly = true;
+                    c.Text = "0";
+                }
+            }
+            FilSummery();
+        }
+        public void FilData(TextBox data1, TextBox data2, TextBox data3)
+        {
+            int count = Convert.ToInt32(data1.Text);
+            if (count == 33)
+            {
+                count = 0;
+                int loop = Convert.ToInt32(data2.Text);
+                loop++;
+                data2.Text = loop+"";
+            }
+            count++;
+            data1.Text = count+"";
+            int total = Convert.ToInt32(data3.Text);
+            total++;
+            data3.Text = total+"";
+            FilSummery();
+        }
+        string FileName = "APPData.txt";
+        public void Save()
+        {
+            StreamWriter BackupData= new StreamWriter(FileName);            
+            BackupData.WriteLine(TXT1_1.Text+","+TXT2_1.Text+","+TXT3_1.Text+"\n"+ TXT1_2.Text + "," + TXT2_2.Text + "," + TXT3_2.Text + "\n"+ TXT1_3.Text + "," + TXT2_3.Text + "," + TXT3_3.Text);
+            BackupData.Close();
+        }
+        private void BTNClear_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+        }
+        private void BTN1_Click(object sender, System.EventArgs e)
+        {
+            FilData(TXT1_1,TXT2_1,TXT3_1);
+}
+        private void BTN2_Click(object sender, EventArgs e)
+        {
+            FilData(TXT1_2,TXT2_2,TXT3_2);
+        }
+        private void BTN3_Click(object sender, EventArgs e)
+        {
+            FilData(TXT1_3,TXT2_3,TXT3_3);
+        }
+        private void BTNSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+        private void BTNExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void HomeForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (MessageBox.Show("هل تريد حفظ التسبيحات الجديدة؟","خروج",MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button1)==DialogResult.Yes)
+            {
+                Save();
+            }
+            Application.Exit();
+        }
+    }
+}
